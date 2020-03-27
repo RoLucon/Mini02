2de8bc57809ac73302c6f2ae996b6d0e8930cbfc
Rogerio Lucon <rogerios.lucon@gmail.com>
2020-03-27 17:25:59 -0300

    Initial Commit

---
+++ Mini02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mini02
+//
+//  Created by Rogerio Lucon on 27/03/20.
+//  Copyright © 2020 Rogerio Lucon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
