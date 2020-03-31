6e31238fd1be9f5b3a643ad016df819b1977810f
gustavo rigor <gusrigor@icloud.com>
2020-03-31 15:00:33 -0300

    Adicionadas funcoes para atulizar os valores na tela inicial e a imagem da personagem na tela de intro

--- Mini02/ViewController.swift
+++ Mini02/ViewController.swift
@@ -13,26 +13,28 @@ class ViewController: UIViewController {
     
     
     var dindin = 0.00
+    var fala = "Oi, Eu sou o Kleytinho"
     
-    
+    //Fala da personagem no campo da tela de intro
     @IBOutlet weak var FalaPrsonagem: UITextView!
-    
+    //A quantidade de dinheiro da personagem no jogo
     @IBOutlet weak var dinheiro: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         atualizaSaldo()
+        atualizaFala()
         // Do any additional setup after loading the view.
     }
-
+    //Funcao para atualizar o saldo ao iniciar a tela
     func atualizaSaldo(){
         dinheiro.text = "R$"+String(dindin)+"0 "
     }
-    
-    @IBAction func DigaOi(_ sender: UIButton) {
-        
-        FalaPrsonagem.text=" Oi, Eu sou o Kleytinho"
+    //Funcao para atualizar a fala da personagem ao carregar a tela
+    func atualizaFala(){
+        FalaPrsonagem.text=fala
     }
+
     
 
 }
