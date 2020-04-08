3ef2758868f66cecf3aa59a9c0ac284b6156eaa8
Rogerio Lucon <rogerios.lucon@gmail.com>
2020-04-08 14:58:57 -0300

    Foi implementado na animacao do text um controle de velocidade.

--- Mini02/Lib/TextLabel.swift
+++ Mini02/Lib/TextLabel.swift
@@ -15,12 +15,17 @@ class TextLabel: UILabel {
         }}
     var counter = 0
     var timer: Timer?
+    var humamMode: Bool = true
+    var speed: Double = 20 {didSet {speed = speed * 10}}
     
     @objc func typeWriter(){
+        var interval = speed * 0.006
         if counter < texto.count {
             let array = Array(texto)
             self.text = self.text! + String(array[counter])
-            let interval = Double((arc4random_uniform(8) + 1)) / 20
+            if humamMode {
+                interval = Double.random(in: 1...8) / (speed * 2)
+            }
             timer?.invalidate()
             timer = Timer.scheduledTimer(timeInterval: TimeInterval(interval), target: self, selector: #selector(typeWriter), userInfo: nil, repeats: true)
         } else {
