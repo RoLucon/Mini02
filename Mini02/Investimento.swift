8d07f5180b33bc74b3f48f67a3ee2d3bf6b9c6c4
gustavo rigor <gusrigor@icloud.com>
2020-04-13 17:21:52 -0300

    Modificado os dados e tratamento do valor liquido do investimento

--- Mini02/Investimento.swift
+++ Mini02/Investimento.swift
@@ -21,17 +21,22 @@ class Investimento {
             UserDefaults.standard.set(aplicada, forKey: "Investimento.aplicada")
         }
     }
-    var imposto = [2.00,0.00,1.00,2.00] {
+    private var liquido = [13.00,23.00,34.00,28.00] {
+        didSet {
+            UserDefaults.standard.set(aplicada, forKey: "Investimento.liquido")
+        }
+    }
+    private var imposto = [2.00,0.00,1.00,2.00] {
         didSet {
             UserDefaults.standard.set(imposto, forKey: "Investimento.imposto")
         }
     }
-    var bruto = [15.00,23.00,35.00,30.00] {
+    private var bruto = [15.00,23.00,35.00,30.00] {
         didSet {
             UserDefaults.standard.set(bruto, forKey: "Investimento.bruto")
         }
     }
-    var lucro = [0.0,0.0,0.0,0.0] {
+    private var lucro = [0.0,0.0,0.0,0.0] {
         didSet {
             UserDefaults.standard.set(lucro, forKey: "Investimento.lucro")
         }
@@ -55,6 +60,9 @@ class Investimento {
         if (UserDefaults.standard.object(forKey: "Investimento.lucro") != nil){
             lucro = (UserDefaults.standard.object(forKey: "Investimento.lucro") as? [Double])!
         }
+        if (UserDefaults.standard.object(forKey: "Investimento.liquido") != nil){
+            liquido = (UserDefaults.standard.object(forKey: "Investimento.liquido") as? [Double])!
+        }
         
     }
     
@@ -67,6 +75,7 @@ class Investimento {
         UserDefaults.standard.removeObject(forKey: "nvestimento.imposto")
         UserDefaults.standard.removeObject(forKey: "Investimento.bruto")
         UserDefaults.standard.removeObject(forKey: "Investimento.lucro")
+        UserDefaults.standard.removeObject(forKey: "Investimento.liquido")
     }
     func getAplicada() -> Double{
         return aplicada[investimentoSelecionado]
@@ -91,6 +100,12 @@ class Investimento {
     }
     func setLucro(_ x:Double){
         lucro[investimentoSelecionado] = x
+    }
+    func getLiquido() -> Double{
+        return liquido[investimentoSelecionado]
+    }
+    func setLiquido(_ x:Double){
+        liquido[investimentoSelecionado] = x
     }
 }
 
