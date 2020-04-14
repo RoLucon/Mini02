38d73e69058c09101ea5b697f8b1a28385fb8608
gustavo rigor <gusrigor@icloud.com>
2020-04-14 16:41:58 -0300

    Adicionada as informacoes ao botao de ajuda da tela de investimentos

--- Mini02/PrincipalViewController.swift
+++ Mini02/PrincipalViewController.swift
@@ -97,15 +97,30 @@ class Investimentos: UIViewController {
     
     @IBOutlet weak var trianguloConstraints: NSLayoutConstraint!
     
+    @IBOutlet weak var efeitoConstraints: NSLayoutConstraint!
+    
+    @IBOutlet var telaAjuda: UIView!
+    
+    @IBOutlet weak var efeito: UIVisualEffectView!
+    
+    @IBOutlet weak var textoAjuda: UILabel!
+    
+    let TextAjuda = "Está é a tela de investimentos. Aqui vai ser possível aplicar o seu dinheiro e ver ele rendendo com o passar do tempo. Todos os investimentos são rendas fixas, isso significa que você receberá juros por ter esta aplicação. Cada um dos investimenros terá uma explicção mais detalhada, basta clicar sobre algum deles. O investimento será de grande ajuda no decorrer da história, além de uma ótima maneira de guardar o seu dinheiro na vida real e receber por isso."
+    
     var Explicacao = ["CDB - O Certificado de Depósito Bancário é um tipo de investimento em renda fixa oferecidos por bancos e com a proteção do Fundo Garantidor de Créditos. Este investimento é de baixo risco e sem taxas de administração, mas com a necessidade de ser declarado no imposto de renda. O CDB é, na prática, um empréstimo de dinheiro ao banco, e este te paga com juros. E este juros, o lucro do investimento, é acordado com a instituição financeira quando o CDB é adquirido. (Colocar a regra do jogo, pelo que é apresentado nas pesquisa que fiz, no ano passado o lucro médio de um bom CDB estava em torno 15 a 20% dependendo do tempo do investimento. Eu gostaria de deixar mais ou menos entre 1 e 1,5% ao mês). Sendo que o Imposto de renda é de 15% (na vida real este valor varia com o tempo em que a pessoa fica com o CDB Variando de 22,5 a 15%) em cima do lucro que você tiver neste investimento.","LCI/LCA - Letras de Crédito Imobiliário e Letras de Crédito do Agronegócio é um tipo de investimento em renda fixa oferecidos por bancos e com a proteção do Fundo Garantidor de Créditos. Este investimento é de baixo risco e sem taxas. Os LCI e LCA são na prática, um empréstimo destinado ao setor do agronegócio e do setor imobiliário. O lucro do investimento é acordado com a instituição financeira quando é adquirido. (Colocar a regra do jogo, pelo que é apresentado nas pesquisa que fiz, no ano passado o lucro médio de um bom LCI/LCA estava em torno 5 a 10% dependendo do tempo do investimento. Eu gostaria de deixar mais ou menos entre 0,5 e 1% ao mês). Não há impostos neste tipo de investimento, ou seja, o valor líquido é o mesmo que o valor bruto.","CRI/CRA - Certificado de Recebíveis Imobiliários e Certificado de Recebíveis do Agronegócio é um tipo de investimento em renda fixa oferecidos por securitizadoras. Os CRI e CRA são, na prática, um empréstimo destinado ao agronegócio ao setor imobiliário para que estes setores tenham capital, por exemplo, para realizar o financiamento de um imóvel. O lucro do investimento é acordado com a corretora quando é adquirido. Este investimento  não é protegido pelo Fundo Garantidor de Créditos o que pode significar perdas financeiras caso os devedores não cumpram sua parte no acordo. (Colocar a regra do jogo, pelo que é apresentado nas pesquisa que fiz, não achei um valor certo do valor do lucro, aparentemente porque depende do tempo que está investindo e da confiabilidade que a securitizadora tem, pois pode haver um calote. Os dados que eu encontrei são baseados no CDI, que é um valor que flutua. Eu gostaria de deixar mais ou menos entre 1,5 e 2% ao mês). Não há impostos neste tipo de investimento, mas pode haver uma cobrança de uma taxa administrativa da corretora (Os valores estão na casa de 2% do valor investido).","Debêntures - Debêntures é um tipo de investimento em renda fixa oferecidos por corretoras. As debêntures são, na prática, um empréstimo para empresas que estão arrecadando recursos para financiar o desenvolvimento de seu negócio. O lucro do investimento é acordado com a corretora quando é adquirido. Este investimento  não é protegido pelo Fundo Garantidor de Créditos o que pode significar perdas financeiras caso os devedores não cumpram sua parte no acordo. (Colocar a regra do jogo, pelo que é apresentado nas pesquisa que fiz, no ano passado o lucro médio de um bom Debênture estava em torno 10 a 20% dependendo do tempo do investimento. Eu gostaria de deixar mais ou menos 1,5 % ao mês). Sendo que o Imposto de renda é de 15% (na vida real este valor varia com o tempo em que a pessoa fica com o CDB Variando de 22,5 a 15%) em cima do lucro que você tiver neste investimento."]
     
     deinit {
         NotificationCenter.default.removeObserver(self)
     }
     
+    var effeito:UIVisualEffect!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        textoAjuda.text = TextAjuda
+        effeito = efeito.effect
+        efeito.effect = nil
+        telaAjuda.layer.cornerRadius = 5
         investimento.investimentoSelecionado = 0
         atualizaRendimento()
         atualizaMsg()
@@ -168,8 +183,38 @@ class Investimentos: UIViewController {
         aplicado.text = String(format: "R$ %.2f", investimento.getLiquido())
         lucro.text = String(format: "R$ %.2f",investimento.getLucro())
     }
+    
     func atualizaMsg(){
         explicacao.text = Explicacao[investimento.investimentoSelecionado]
+    }
+    func adicionaEfeito(){
+        efeitoConstraints.constant = 0
+        self.view.addSubview(telaAjuda)
+        telaAjuda.center = self.view.center
+        telaAjuda.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
+        telaAjuda.alpha = 0
+        UIView.animate(withDuration: 0.4){
+            self.efeito.effect = self.effeito
+            self.telaAjuda.alpha = 1
+            self.telaAjuda.transform = CGAffineTransform.identity
+        }
+    }
+    func retiraEfeito(){
+        efeitoConstraints.constant = -414
+        UIView.animate(withDuration: 0.3, animations:{
+            self.telaAjuda.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
+            self.telaAjuda.alpha = 0
+            self.efeito.effect = nil
+        }) { (success: Bool) in
+            self.telaAjuda.removeFromSuperview()
+        }
+    }
+    
+    @IBAction func ajudaInvestimento(_ sender: UIButton) {
+        adicionaEfeito()
+    }
+    @IBAction func ajudaFechar(_ sender: UIButton) {
+        retiraEfeito()
     }
     
 }
