9f3019c57bb0932bc374199f58148fde815343bb
Rogerio Lucon <rogerios.lucon@gmail.com>
2020-04-27 16:44:40 -0300

    Narrativa trabalhando com structs, transicao de imagens com animacao.
    Imagens temporarias na narrativa.

--- Mini02/ViewController/NarrativaViewController.swift
+++ Mini02/ViewController/NarrativaViewController.swift
@@ -18,7 +18,8 @@ class NarrativaViewController : UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        carregaTexto()
+//        carregaTexto()
+        geraCenas()
         label.text = ""
         passarCena(self)
     }
@@ -30,13 +31,10 @@ class NarrativaViewController : UIViewController {
     }
     
     @IBAction func passarCena(_ sender: Any) {
-        //Utilizado com cenas quando imgs estiverem prontas
-//        if index < texto.count {
-//        label.texto = cenas[index].texto!
-//        imgView.changeImage(imageTo: cenas[index].img!)
-//        index += 1
-        if index < texto.count {
-            label.texto = texto[index]
+
+        if index < cenas.count {
+            label.texto = cenas[index].texto!
+            imgView.changeImage(imageTo: cenas[index].img!)
             index += 1
         } else {
             let storyboard = UIStoryboard(name: "Principal", bundle: nil)
@@ -45,16 +43,21 @@ class NarrativaViewController : UIViewController {
             self.present(viewController, animated: true, completion: nil)
         }
     }
-    // Lista de Imagens
-    // 1 - olhandoHorizonte
-    // 2 - dormindoSofa
-    // 3 -
+    // Lista de Imagens ---*  Substituir nome das Imagens pelas Finais  *---
+    // 1 - olhando Horizonte
+    // 2 - dormindo Sofa com salgadinhos e controle
+    // 3 - No Busao olhando pela janela
+    // 4 - No escritorio - Cozinhando
+    // 5 - Kleytinho desesperado, dinheiro caindo
+    // 6 - Kim mostrando a lingua
+    // 7 - Entusiasmado
     func geraCenas(){
-        cenas = [Cena(texto: "Era uma vez menino Kleytinho, um jovem recém-formado do ensino médio que sonha em ser um astronauta.", imgName: "olhandoHorizonte"), Cena(texto: "Todos os dias, ele ficava encostado na casa dos pais, sem ajudar nas tarefas econômicas da família.", imgName: "dormindoSofa")]
-    }
-    //Substituido pelas cenas quando imgs estiverem prontas
-    func carregaTexto() {
-        self.texto = ["Era uma vez menino \(Personagem.shared.nome!), um jovem recém-formado do ensino médio que sonha em ser um astronauta.","Todos os dias, ele ficava encostado na casa dos pais, sem ajudar nas tarefas econômicas da família.","Mas um dia ele precisou se mudar para outra cidade para estudar Astrofísica.\nPor causa disso, conseguiu seu primeiro emprego e uma moradia em uma república.","Por nunca ter se sustentado antes, se afundou em dívidas…","Mas sua colega de república, Kim, manja muito de economia e vai ajudá-lo a sair dessa!…Ou tentar."]
+        cenas = [Cena(texto: "Era uma vez menino Kleytinho, um jovem recém-formado do ensino médio que sonha em ser um astronauta.", imgName: "kim"),
+                 Cena(texto: "Todos os dias, ele ficava encostado na casa dos pais, sem ajudar nas tarefas econômicas da família.", imgName: "kimTEMPORARIO"),
+                 Cena(texto: "Mas um dia ele precisou se mudar para outra cidade para estudar Astrofísica.\nPor causa disso, conseguiu seu primeiro emprego e uma moradia em uma república.", imgName: "kim"),
+                 Cena(texto: "Por causa disso, conseguiu seu primeiro emprego e uma moradia em uma república.", imgName: "kimTEMPORARIO"),
+                 Cena(texto: "Mas sua colega de república, Kim, manja muito de economia e vai ajudá-lo a sair dessa!", imgName: "kim"),
+                 Cena(texto: "…Ou tentar.", imgName: "kimTEMPORARIO")]
     }
     
     struct Cena {
