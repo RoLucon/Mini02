bcfb36cb670b929ded7d7da16dd34f7c79e63344
Rogerio Lucon <rogerios.lucon@gmail.com>
2020-04-27 14:15:31 -0300

    Preparando prologo para trabalhar com struct com texto e imagem.

--- Mini02/ViewController/NarrativaViewController.swift
+++ Mini02/ViewController/NarrativaViewController.swift
@@ -10,16 +10,17 @@ import UIKit
 
 class NarrativaViewController : UIViewController {
     
+    var cenas: [Cena]!
     var texto: [String]!
     var index = 0
     @IBOutlet weak var label: TextLabel!
+    @IBOutlet weak var imgView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         carregaTexto()
         label.text = ""
-        passarTexto(self)
-        
+        passarCena(self)
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -28,7 +29,12 @@ class NarrativaViewController : UIViewController {
         label.speed = 4
     }
     
-    @IBAction func passarTexto(_ sender: Any) {
+    @IBAction func passarCena(_ sender: Any) {
+        //Utilizado com cenas quando imgs estiverem prontas
+//        if index < texto.count {
+//        label.texto = cenas[index].texto!
+//        imgView.image = cenas[index].img
+//        index += 1
         if index < texto.count {
             label.texto = texto[index]
             index += 1
@@ -39,8 +45,25 @@ class NarrativaViewController : UIViewController {
             self.present(viewController, animated: true, completion: nil)
         }
     }
-    
+    // Lista de Imagens
+    // 1 - olhandoHorizonte
+    // 2 - dormindoSofa
+    // 3 -
+    func geraCenas(){
+        cenas = [Cena(texto: "Era uma vez menino Kleytinho, um jovem recém-formado do ensino médio que sonha em ser um astronauta.", imgName: "olhandoHorizonte"), Cena(texto: "Todos os dias, ele ficava encostado na casa dos pais, sem ajudar nas tarefas econômicas da família.", imgName: "dormindoSofa")]
+    }
+    //Substituido pelas cenas quando imgs estiverem prontas
     func carregaTexto() {
         self.texto = ["Era uma vez menino Kleytinho, um jovem recém-formado do ensino médio que sonha em ser um astronauta.","Todos os dias, ele ficava encostado na casa dos pais, sem ajudar nas tarefas econômicas da família.","Mas um dia ele precisou se mudar para outra cidade para estudar Astrofísica.\nPor causa disso, conseguiu seu primeiro emprego e uma moradia em uma república.","Por nunca ter se sustentado antes, se afundou em dívidas…","Mas sua colega de república, Kim, manja muito de economia e vai ajudá-lo a sair dessa!…Ou tentar."]
+    }
+    
+    struct Cena {
+        let texto: String?
+        let img: UIImage?
+        
+        init(texto: String, imgName: String) {
+            self.texto = texto
+            self.img = UIImage(named: imgName)
+        }
     }
 }
